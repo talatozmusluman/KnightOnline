fbf6d65eb654712e609b7b32f7065c2dc766aa3d
JD <john@doe.com>
2026-02-17 23:48:25 +0300

    Rebuild core libraries in stack script

--- build_scripts/rebuild_stack.cmd
+++ build_scripts/rebuild_stack.cmd
@@ -22,19 +22,37 @@ IF ERRORLEVEL 1 EXIT /B 1
 CALL "%~dp0msbuild.cmd" "src\Server\shared-server\shared-server.vcxproj" %COMMON%
 IF ERRORLEVEL 1 EXIT /B 1
 
+CALL "%~dp0msbuild.cmd" "src\Server\AIServer\AIServer.Core.vcxproj" %COMMON%
+IF ERRORLEVEL 1 EXIT /B 1
+
 CALL "%~dp0msbuild.cmd" "src\Server\AIServer\AIServer.vcxproj" %COMMON%
+IF ERRORLEVEL 1 EXIT /B 1
+
+CALL "%~dp0msbuild.cmd" "src\Server\Ebenezer\Ebenezer.Core.vcxproj" %COMMON%
 IF ERRORLEVEL 1 EXIT /B 1
 
 CALL "%~dp0msbuild.cmd" "src\Server\Ebenezer\Ebenezer.vcxproj" %COMMON%
 IF ERRORLEVEL 1 EXIT /B 1
 
+CALL "%~dp0msbuild.cmd" "src\Server\Aujard\Aujard.Core.vcxproj" %COMMON%
+IF ERRORLEVEL 1 EXIT /B 1
+
 CALL "%~dp0msbuild.cmd" "src\Server\Aujard\Aujard.vcxproj" %COMMON%
+IF ERRORLEVEL 1 EXIT /B 1
+
+CALL "%~dp0msbuild.cmd" "src\Server\ItemManager\ItemManager.Core.vcxproj" %COMMON%
 IF ERRORLEVEL 1 EXIT /B 1
 
 CALL "%~dp0msbuild.cmd" "src\Server\ItemManager\ItemManager.vcxproj" %COMMON%
 IF ERRORLEVEL 1 EXIT /B 1
 
+CALL "%~dp0msbuild.cmd" "src\Server\VersionManager\VersionManager.Core.vcxproj" %COMMON%
+IF ERRORLEVEL 1 EXIT /B 1
+
 CALL "%~dp0msbuild.cmd" "src\Server\VersionManager\VersionManager.vcxproj" %COMMON%
+IF ERRORLEVEL 1 EXIT /B 1
+
+CALL "%~dp0msbuild.cmd" "src\Client\WarFare\WarFare.Core.vcxproj" %COMMON%
 IF ERRORLEVEL 1 EXIT /B 1
 
 CALL "%~dp0msbuild.cmd" "src\Client\WarFare\WarFare.vcxproj" %COMMON%
